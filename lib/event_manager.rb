8a9526e3bfdd9d3a62f49a82a67c4cbda819e31f
matterramun <Veradux4@protonmail.com>
2023-06-02 17:46:50 -0400

    Standardize Zipcodes to be 5 digits.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -25,6 +25,14 @@ contents = CSV.open(
   header_converters: :symbol
 )
 contents.each do |row|
-  name = row[:firstname]
-  puts name
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+
+  if zipcode.length > 5
+    zipcode = zipcode.rjust(5,'0')
+  elsif zipcode.length < 5
+    zipcode = zipcode.slice(0..4)
+  end
+
+  puts "#{name} #{zipcode}"
 end

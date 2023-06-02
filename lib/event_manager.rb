8d530b3c23c4c10b0ce0a301d283a6bc22695dcb
matterramun <Veradux4@protonmail.com>
2023-06-02 17:30:19 -0400

    Replace homebrewed CSV parser with Ruby CSV lib.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,19 +1,26 @@
+require 'csv'
 puts 'Event Manager Initialized!'
 
 if File.exist?('event_attendees.csv')
-    puts "Found it..."
+  puts 'Found it...'
 else
-    puts "I can't find the file!"
-    exit
+  puts "I can't find the file!"
+  exit
 end
 
-#contents = File.read('event_attendees.csv')
-#puts contents
+# contents = File.read('event_attendees.csv')
+# puts contents
 
-lines = File.readlines('event_attendees.csv')
-lines.each.with_index do |line, index|
-    next if index == 0
-    columns = line.split(",")
-    name = columns[2]
-    p name
+# lines = File.readlines('event_attendees.csv')
+# lines.each.with_index do |line, index|
+#    next if index == 0
+#    columns = line.split(",")
+#    name = columns[2]
+#    p name
+# end
+
+contents = CSV.open('event_attendees.csv', headers: true)
+contents.each do |row|
+  name = row[2]
+  puts name
 end

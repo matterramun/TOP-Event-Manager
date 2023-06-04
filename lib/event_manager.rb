02a7047f8b828029e85cc0cdb64766c8f67d80bd
matterramun <Veradux4@protonmail.com>
2023-06-04 14:02:48 -0400

    Implement Clean phone number and replace HTML with ERB.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,49 +6,52 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
-def legislators_by_zipcode(zipcode)
+def clean_phone_num(phone)
+  bad_phone = ""
+  phone = "%f" % phone if phone.include? "+"
+  phone.gsub!(/[^0-9]/,"")
+  case phone.length
+  when 12..(1.0/0.0)
+    bad_phone
+  when 0..9
+    bad_phone
+  when 11
+    if phone.index('1') == 0
+      phone[1..-1]
+    else
+      bad_phone
+    end
+  when 10
+    phone
+  end
+end
+
+def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
-      address: zipcode,
+    civic_info.representative_info_by_address(
+      address: zip,
       levels: 'country',
-      roles: %w[legislatorUpperBody legislatorLowerBody]
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names.join(', ')
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
-def build_letter(name, legislators)
-  template_letter = File.read('form_letter.html')
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
 
-  contents.each do |row|
-    name = row[:first_name]
+  filename = "output/thanks_#{id}.html"
 
-    zipcode = clean_zipcode(row[:zipcode])
-
-    legislators = legislators_by_zipcode(zipcode)
-
-    personal_letter = template_letter.gsub('FIRST_NAME', name)
-    personal_letter.gsub!('LEGISLATORS', legislators)
-
-    puts personal_letter
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
   end
 end
 
-puts 'Event Manager Initialized!'
-
-if File.exist?('event_attendees.csv')
-  puts 'Found it...'
-else
-  puts "I can't find the file!"
-  exit
-end
+puts 'EventManager initialized.'
 
 contents = CSV.open(
   'event_attendees.csv',
@@ -56,12 +59,17 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(zipcode)
+  phone = clean_phone_num(row[:homephone])
 
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+
+  #save_thank_you_letter(id,form_letter)
 end
